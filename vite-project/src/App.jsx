277a08a6e8c45867308cabc784249a9c3fd13382
Rocio Gonzalez <rogq88@gmail.com>
2024-05-23 18:09:00 -0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes/</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App
